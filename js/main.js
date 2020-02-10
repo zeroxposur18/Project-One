97573b29b851dcae9390ba373c2e13fea65da3ba
Tan Vu <vtan502262@gmail.com>
2020-02-10 12:32:13 -0600

    Clean up

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ const squareThree = document.querySelector("#sq3");
 const squareFour = document.querySelector("#sq4");
 
 //Event listener set up
-document.querySelector('.board').addEventListener('click', onClick);
 document.querySelector('button').addEventListener('click', btnClick);
 squareOne.addEventListener('click', oneClick);
 squareTwo.addEventListener('click', twoClick);
@@ -25,9 +24,26 @@ squareFour.addEventListener('click', fourClick);
 function btnClick(e){
     console.log("Shit button works too");
     start();
-}
-function onClick(e){
-    console.log("shit it works");
+};
+
+function oneClick(){
+    console.log("Shit button one works");
+    userArray.push(1);
+};
+
+function twoClick(){
+    console.log("Shit button two works");
+    userArray.push(2);
+};
+
+function threeClick(){
+    console.log("Shit button three works");
+    userArray.push(3);
+};
+
+function fourClick(){
+    console.log("Shit button four works");
+    userArray.push(4)
 };
 
 //Functions
@@ -35,22 +51,7 @@ function start(){
     gameRandom()
     console.log(gameArray);
 };
-function oneClick(){
-    console.log("Shit button one works");
-    userArray.push(1);
-};
-function twoClick(){
-    console.log("Shit button two works");
-    userArray.push(2);
-};
-function threeClick(){
-    console.log("Shit button three works");
-    userArray.push(3);
-};
-function fourClick(){
-    console.log("Shit button four works");
-    userArray.push(4)
-};
+
 function gameRandom(){
     gameArray.push(Math.floor(Math.random()*4)+ 1);
-}
+};
