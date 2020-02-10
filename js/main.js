f71a8d27c32319abc0c7299a793ba8e4adc520c3
Tan Vu <vtan502262@gmail.com>
2020-02-10 12:15:23 -0600

    Remove board array due to no use

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Variables
-var board = [1,2,3,4]
 var userArray =[];
 var gameArray =[];
 var gameCount;
@@ -13,6 +12,7 @@ const squareOne = document.querySelector("#sq1");
 const squareTwo = document.querySelector("#sq2");
 const squareThree = document.querySelector("#sq3");
 const squareFour = document.querySelector("#sq4");
+
 //Event listener set up
 document.querySelector('.board').addEventListener('click', onClick);
 document.querySelector('button').addEventListener('click', btnClick);
@@ -20,6 +20,7 @@ squareOne.addEventListener('click', oneClick);
 squareTwo.addEventListener('click', twoClick);
 squareThree.addEventListener('click', threeClick);
 squareFour.addEventListener('click', fourClick);
+
 //onclick function
 function btnClick(e){
     console.log("Shit button works too");
@@ -32,19 +33,24 @@ function onClick(e){
 //Functions
 function start(){
     gameRandom()
+    console.log(gameArray);
+};
+function oneClick(){
+    console.log("Shit button one works");
+    userArray.push(1);
+};
+function twoClick(){
+    console.log("Shit button two works");
+    userArray.push(2);
+};
+function threeClick(){
+    console.log("Shit button three works");
+    userArray.push(3);
+};
+function fourClick(){
+    console.log("Shit button four works");
+    userArray.push(4)
 };
 function gameRandom(){
     gameArray.push(Math.floor(Math.random()*4)+ 1);
 }
-function oneClick(){
-    console.log("Shit button one works")
-};
-function twoClick(){
-    console.log("Shit button two works")
-};
-function threeClick(){
-    console.log("Shit button three works")
-};
-function fourClick(){
-    console.log("Shit button four works")
-};
