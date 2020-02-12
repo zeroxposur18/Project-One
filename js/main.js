5c79398ebc9e1458f8e2da11a89ca7fef44c5172
Tan Vu <vtan502262@gmail.com>
2020-02-12 15:16:50 -0600

    Code Clean up

--- js/main.js
+++ js/main.js
@@ -10,23 +10,27 @@ var timer;
 var functionTempo;
 var tempoOne;
 var tempoTwo;
+
 //Cached Elements
 const squareOne = document.querySelector("#sq1");
 const squareTwo = document.querySelector("#sq2");
 const squareThree = document.querySelector("#sq3");
 const squareFour = document.querySelector("#sq4");
+
 //Event listener set up
 document.querySelector('button').addEventListener('click', btnClick);
 squareOne.addEventListener('click', oneClick);
 squareTwo.addEventListener('click', twoClick);
 squareThree.addEventListener('click', threeClick);
 squareFour.addEventListener('click', fourClick);
+
 //onclick function
 function btnClick(e){
     if (count === 0)
         userArray =[];
         start();
 };
+
 function oneClick(){
     userArray.push(1);
     audioClear();
@@ -37,6 +41,7 @@ function oneClick(){
         audioClear();
     },600)
 };
+
 function twoClick(){
     userArray.push(2);
     audioClear();
@@ -47,6 +52,7 @@ function twoClick(){
         audioClear();
     },600) 
 };
+
 function threeClick(){
     userArray.push(3);
     audioClear();
@@ -57,6 +63,7 @@ function threeClick(){
         audioClear();
     },600)
 };
+
 function fourClick(){
     userArray.push(4)
     audioClear();
@@ -67,6 +74,7 @@ function fourClick(){
         audioClear();
     },600)
 };
+
 //Function definitions
 function one(){
     audioClear();
@@ -77,6 +85,7 @@ function one(){
         audioClear();
     },functionTempo)
 };
+
 function two(){
     audioClear();
     document.getElementById('audio-two').play();
@@ -85,7 +94,8 @@ function two(){
         colorClear();
         audioClear();
     },functionTempo) 
-};1
+};
+
 function three(){
     audioClear();
     document.getElementById('audio-three').play();
@@ -95,6 +105,7 @@ function three(){
         audioClear();
     },functionTempo)
 };
+
 function four(){
     audioClear();
     document.getElementById('audio-four').play();
@@ -104,12 +115,14 @@ function four(){
         audioClear();
     },functionTempo)
 };
+
 function start(){
     document.getElementById('audio-start').volume = .3;
     document.getElementById('audio-start').play();
     gameRandom();
     gameFlash();
 };
+
 function gameRandom(){  
     gameArray.push(Math.floor(Math.random()*4)+ 1);
     count++;
@@ -117,6 +130,7 @@ function gameRandom(){
     userTempo();
     gameTempo();
 };
+
 function gameFlash(){
     document.getElementById('audio-one').play();
     document.getElementById('audio-four').play();
@@ -138,21 +152,23 @@ function gameFlash(){
     userTurn();
 }, (count + 1)*tempoOne);
 };
+
 function userTurn() {
-playerTurn = true;
-document.getElementById('audio-two').play();
-document.getElementById('audio-four').play();
-timer = setInterval(function(){
+    playerTurn = true;
+    document.getElementById('audio-two').play();
+    document.getElementById('audio-four').play();
+    timer = setInterval(function(){
     document.querySelector('button').innerHTML =`<h1> ${timeLeft}   seconds remaining. </h1>`;
     timeLeft -= 1;
     if (timeLeft < 0){clearInterval(timer)}
 }, 1000)
-if (playerTurn = true) {setTimeout(function() {
+    if (playerTurn = true) {setTimeout(function() {
     if (userArray.length === gameArray.length){
         userCheck();}
     else gameOver();
-},tempoTwo)}
+    },tempoTwo)}
 };
+
 function userCheck() {
     playerTurn = false;
     arrayCheck();
@@ -169,16 +185,19 @@ function userCheck() {
     }
     else gameOver();
 };
+
 function arrayCheck(){
     for (var i = 0; i < count; i++) {
         if (userArray[i] === gameArray[i]){
         matchArray = true;}
         else matchArray = false;
     }};
+
 function userTempo(){
     tempoTwo = (count + 1)* 1000;
     timeLeft = (count);
 };
+
 function gameTempo(){
     if (count >= 1 && count < 3){
         tempoOne = 1000;
@@ -190,6 +209,7 @@ function gameTempo(){
         tempoOne = 600;
     }
 };
+
 function flashTempo(){
     if (count >= 1 && count < 3){
         functionTempo = 800;
@@ -201,12 +221,14 @@ function flashTempo(){
         functionTempo = 500;
     }  
 };
+
 function colorClear() {
     squareOne.style.backgroundColor = 'darkgreen';
     squareTwo.style.backgroundColor = 'darkred';
     squareThree.style.backgroundColor = 'darkgoldenrod';
     squareFour.style.backgroundColor = 'darkblue';
 };
+
 function audioClear(){
     document.getElementById('audio-one').pause();
     document.getElementById('audio-two').pause();
@@ -217,6 +239,7 @@ function audioClear(){
     document.getElementById('audio-three').currentTime = 0;
     document.getElementById('audio-four').currentTime = 0;
 };
+
 function gameOver(){
     document.getElementById('audio-one').play();
     setTimeout(function(){
@@ -236,6 +259,7 @@ function gameOver(){
     count = 0;
     playerTurn = false;
 };
+
 function startAgain(){
     gameRandom();
     gameFlash();
